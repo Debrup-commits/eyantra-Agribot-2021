f552f634750ea563168e856060159a4f30a71894
Debrup-commits <120cs0201@nitrkl.ac.in>
2021-10-13 15:54:34 +0530

    code changed slightly

--- pkg_task0/scripts/node_turtle_revolve.py
+++ pkg_task0/scripts/node_turtle_revolve.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import cv2
+from cv_bridge import CvBridge, CvBridgeError
 import sys
 
 class Revolve:
     def __init__(self):
-        rospy.init_node('node_turtle_revolve')
+        rospy.init_node('turtlesim_revolver')
 
         self.sub = rospy.Subscriber('/turtle1/pose', Pose, self.callback)
-        self.pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 1)
+        self.pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
 
         self.rate = rospy.Rate(10)
-        self.flag=0
 
         self.bot_pose = 0
 
@@ -23,42 +23,46 @@ class Revolve:
 
     def callback(self, data):
 
-        if not rospy.is_shutdown():
+        try:
             self.pose = data
-            x = round(self.pose.x, 4)
-            y = round(self.pose.y, 4)
-            z = round(self.pose.theta, 4)
-            if(z>0.3):
-                self.flag = 1
-            if(z>1.34):
-                self.flag = -1
+            a = self.pose.theta
 
-
-            print(x, y,z)
-            print(self.flag)
-            if(self.flag==0):
-                self.vel.linear.x = 3
-                self.vel.angular.z = 3
-            elif self.flag==-1:
-                self.vel.linear.x = 0
-                self.vel.angular.z = 0
-            else:
+            if self.bot_pose == 0:
                 self.vel.linear.x = 3
                 self.vel.angular.z = -3
-            
+
+                if 0 < a < 0.07:
+                    self.bot_pose +=1
+
+            if self.bot_pose == 1:
+                self.vel.linear.x = 3
+                self.vel.angular.z = 3
+
+                if -0.07 < a < 0:
+                    self.bot_pose += 1
+
+            if self.bot_pose == 2:
+                self.vel.linear.x = 0
+                self.vel.angular.z = 0
+
             self.pub.publish(self.vel)
 
-            rospy.loginfo("Moving in a circle")
-            self.rate.sleep()
+            if self.bot_pose < 2:
+                rospy.loginfo("Moving in a circle")
+            else:
+                rospy.loginfo("Target reached")
+                rospy.signal_shutdown("Task 0 completed")
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 rospy.signal_shutdown("user command")
+
+        except CvBridgeError as e:
+            print(e)
 
 if __name__== '__main__':
     rv = Revolve()
     try:
         if not rospy.is_shutdown():
             rospy.spin()
-            rospy.loginfo("shutdown....")
     except rospy.ROSInterruptException as e:
         print(e)
