7939c72d43141aecd8e5ac9a8f425a8339cdb1df
Debrup-commits <120cs0201@nitrkl.ac.in>
2021-11-24 19:11:25 +0530

    routine push

---
+++ agribot_tasks/ebot_nav/scripts/go_to_pose.py
@@ -0,0 +1,143 @@
+#! /usr/bin/env python3
+
+import rospy
+import sys
+import copy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import actionlib
+import math
+
+
+class Ur5Moveit:
+
+    # Constructor
+    def __init__(self):
+
+        rospy.init_node('node_eg3_set_joint_angles', anonymous=True)
+
+        self._planning_group = "arm"
+        self._commander = moveit_commander.roscpp_initialize(sys.argv)
+        self._robot = moveit_commander.RobotCommander()
+        self._scene = moveit_commander.PlanningSceneInterface()
+        self._group = moveit_commander.MoveGroupCommander(self._planning_group)
+        self._display_trajectory_publisher = rospy.Publisher(
+            '/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=1)
+
+        self._exectute_trajectory_client = actionlib.SimpleActionClient(
+            'execute_trajectory', moveit_msgs.msg.ExecuteTrajectoryAction)
+        self._exectute_trajectory_client.wait_for_server()
+
+        self._planning_frame = self._group.get_planning_frame()
+        self._eef_link = self._group.get_end_effector_link()
+        self._group_names = self._robot.get_group_names()
+        self._box_name = ''
+
+        # Current State of the Robot is needed to add box to planning scene
+        self._curr_state = self._robot.get_current_state()
+
+        rospy.loginfo(
+            '\033[94m' + "Planning Group: {}".format(self._planning_frame) + '\033[0m')
+        rospy.loginfo(
+            '\033[94m' + "End Effector Link: {}".format(self._eef_link) + '\033[0m')
+        rospy.loginfo(
+            '\033[94m' + "Group Names: {}".format(self._group_names) + '\033[0m')
+
+        rospy.loginfo('\033[94m' + " >>> Ur5Moveit init done." + '\033[0m')
+
+    def go_to_pose(self, arg_pose):
+
+        pose_values = self._group.get_current_pose().pose
+        rospy.loginfo('\033[94m' + ">>> Current Pose:" + '\033[0m')
+        rospy.loginfo(pose_values)
+
+        self._group.set_pose_target(arg_pose)
+        flag_plan = self._group.go(wait=True)  # wait=False for Async Move
+
+        pose_values = self._group.get_current_pose().pose
+        rospy.loginfo('\033[94m' + ">>> Final Pose:" + '\033[0m')
+        rospy.loginfo(pose_values)
+
+        list_joint_values = self._group.get_current_joint_values()
+        rospy.loginfo('\033[94m' + ">>> Final Joint Values:" + '\033[0m')
+        rospy.loginfo(list_joint_values)
+
+        if (flag_plan == True):
+            rospy.loginfo(
+                '\033[94m' + ">>> go_to_pose() Success" + '\033[0m')
+        else:
+            rospy.logerr(
+                '\033[94m' + ">>> go_to_pose() Failed. Solution for Pose not Found." + '\033[0m')
+
+        return flag_plan
+
+    def quaternion_from_euler(self, roll, pitch, yaw):
+        """
+        Converts euler roll, pitch, yaw to quaternion (w in last place)
+        quat = [x, y, z, w]
+        Bellow should be replaced when porting for ROS 2 Python tf_conversions is done.
+        """
+        cy = math.cos(yaw * 0.5)
+        sy = math.sin(yaw * 0.5)
+        cp = math.cos(pitch * 0.5)
+        sp = math.sin(pitch * 0.5)
+        cr = math.cos(roll * 0.5)
+        sr = math.sin(roll * 0.5)
+
+        q = [0] * 4
+        q[0] = cy * cp * cr + sy * sp * sr
+        q[1] = cy * cp * sr - sy * sp * cr
+        q[2] = sy * cp * sr + cy * sp * cr
+        q[3] = sy * cp * cr - cy * sp * sr
+
+        return q
+
+    def coordinateTransform(self, target_coordinates, origin_coordinates):
+        transformed_coordinates = [0]*3
+        transformed_coordinates[0] = target_coordinates[0]-origin_coordinates[0]
+        transformed_coordinates[1] = target_coordinates[1]-origin_coordinates[1]
+        transformed_coordinates[2] = target_coordinates[2]-origin_coordinates[2]
+
+        return transformed_coordinates
+
+
+    # Destructor
+    def __del__(self):
+        moveit_commander.roscpp_shutdown()
+        rospy.loginfo(
+            '\033[94m' + "Object of class Ur5Moveit Deleted." + '\033[0m')
+
+
+def main():
+
+    ur5 = Ur5Moveit()
+
+    ur5_pose_1 = geometry_msgs.msg.Pose()
+    ur5_pose_1.position.x = 0.250936364559653
+    ur5_pose_1.position.y = 0.10935813860877695
+    ur5_pose_1.position.z = 0.670607
+
+    # ur5_pose_2 = geometry_msgs.msg.Pose()
+    # ur5_pose_2.position.x = -0.1695751
+    # ur5_pose_2.position.y = 0.203677
+    # ur5_pose_2.position.z = 1.205607
+
+    # ur5_pose_3 = geometry_msgs.msg.Pose()
+    # ur5_pose_3.position.x = 0.107778
+    # ur5_pose_3.position.y = 0.373677
+    # ur5_pose_3.position.z = 0.935141
+
+    while not rospy.is_shutdown():
+        ur5.go_to_pose(ur5_pose_1)
+        rospy.sleep(2)
+        # ur5.go_to_pose(ur5_pose_2)
+        # rospy.sleep(2)
+        # ur5.go_to_pose(ur5_pose_3)
+        # rospy.sleep(2)
+
+    del ur5
+
+
+if __name__ == '__main__':
+    main()
