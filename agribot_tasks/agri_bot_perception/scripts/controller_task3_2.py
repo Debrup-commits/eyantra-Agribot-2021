b7515108b616328dff9682de9dbb79efe2a1a4cc
Debrup-commits <120cs0201@nitrkl.ac.in>
2021-12-30 23:37:35 +0530

    routine push

--- agribot_tasks/agri_bot_perception/scripts/controller_task3_2.py
+++ agribot_tasks/agri_bot_perception/scripts/controller_task3_2.py
@@ -13,6 +13,10 @@ import numpy as np
 import rospy
 from sensor_msgs.msg import Image
 import sys
+import tf2_ros
+import tf_conversions
+import tf
+from tf.transformations import euler_from_quaternion
 
 
 class Ur5Moveit:
@@ -64,17 +68,53 @@ class Ur5Moveit:
         self.image_sub = message_filters.Subscriber("/camera/color/image_raw2", Image)
         self.depth_sub = message_filters.Subscriber("/camera/depth/image_raw2", Image)
 
+        self.rgb_frame = None
+        self.depth_frame = None
+
+        self.get_coordinates = True
+
         self.ts = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.depth_sub], queue_size=10, slop=0.5)
         self.ts.registerCallback(self.image_callback)
+
+        listener = tf.TransformListener()
 
         self.bridge = CvBridge()
 
         while not rospy.is_shutdown():
             self.go_to_predefined_pose("rotate_90_2")
-            moveit_commander.roscpp_shutdown()
+            print("aagya")
+            coordinates = self.give_tomato_coordinates(self.rgb_frame, self.depth_frame)
+            # moveit_commander.roscpp_shutdown()
+            ur5_pose = geometry_msgs.msg.Pose()
+            try:
+                for i, coordinate in enumerate(coordinates):
+                    trans, rot = listener.lookupTransform(target_frame='/base_link', source_frame='/obj{}'.format(i), time=rospy.Time(0))
+                    ur5_pose.position.x = trans[0]
+                    ur5_pose.position.y = trans[1]
+                    ur5_pose.position.z = trans[2]
+                    # ur5_pose.orientation.x = rot[0]
+                    # ur5_pose.orientation.y = rot[1]
+                    # ur5_pose.orientation.z = rot[2]
+                    # ur5_pose.orientation.w = rot[3]
+
+                    print(i)
+
+                    self.go_to_pose(ur5_pose)
+                    self.go_to_predefined_pose("close")
+                    self.go_to_predefined_pose("home")
+                    self.go_to_predefined_pose("open")
+                    # x = input("enter password to continue")
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+
+            # rospy.signal_shutdown("task done")
+
+            # i=0
+            # rospy.signal_shutdown("task3_2 completed")
+            # while i < i+1:
+            #     i+=1
 
     def go_to_predefined_pose(self, arg_pose_name):
-        rospy.loginfo('\033[94m' + "Going to Pose: {}".format(arg_pose_name) + '\033[0m')
 
         if arg_pose_name == "open" or arg_pose_name == "close":
             self._group_2.set_named_target(arg_pose_name)
@@ -87,18 +127,18 @@ class Ur5Moveit:
 
     def go_to_pose(self, arg_pose):
 
-        pose_values = self._group.get_current_pose().pose
+        pose_values = self._group_1.get_current_pose().pose
         rospy.loginfo('\033[94m' + ">>> Current Pose:" + '\033[0m')
         rospy.loginfo(pose_values)
 
-        self._group.set_pose_target(arg_pose)
-        flag_plan = self._group.go(wait=True)  # wait=False for Async Move
+        self._group_1.set_pose_target(arg_pose)
+        flag_plan = self._group_1.go(wait=True)  # wait=False for Async Move
 
-        pose_values = self._group.get_current_pose().pose
+        pose_values = self._group_1.get_current_pose().pose
         rospy.loginfo('\033[94m' + ">>> Final Pose:" + '\033[0m')
         rospy.loginfo(pose_values)
 
-        list_joint_values = self._group.get_current_joint_values()
+        list_joint_values = self._group_1.get_current_joint_values()
         rospy.loginfo('\033[94m' + ">>> Final Joint Values:" + '\033[0m')
         rospy.loginfo(list_joint_values)
 
@@ -111,129 +151,93 @@ class Ur5Moveit:
 
         return flag_plan
 
-    def image_callback(self, rgb_data, depth_data):
-
+    def give_tomato_coordinates(self, rgb_frame, depth_frame):
         # Initializing variables
         focal_length = 554.387
         center_x = 320.5
         center_y = 240.5
 
+        img_HSV = cv2.cvtColor(rgb_frame, cv2.COLOR_BGR2HSV)
+
+        low_HSV = np.array([0, 53, 137])
+        upper_HSV = np.array([10, 255, 255])
+
+        mask = cv2.inRange(img_HSV, low_HSV, upper_HSV)
+        output = cv2.bitwise_and(rgb_frame, rgb_frame, mask = mask)
+        output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+
+        ret, threshold = cv2.threshold(output, 25, 255, cv2.THRESH_BINARY)
+
+        contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        i=0
+
+        coordinates = []
+        coordinate = []
+
+        for contour in contours:
+            C = cv2.moments(contour)
+
+            cX = int(C['m10']/(C['m00']+1e-4))
+            cY = int(C['m01']/(C['m00']+1e-4))
+            depth = depth_frame[cY][cX]
+
+            # transforming pixel coordinates to world coordinates
+            world_x = (cX - center_x)/focal_length*depth
+            world_y = (cY - center_y)/focal_length*depth
+            world_z = depth
+
+            # broadcasting TF for each tomato coordinate
+            br = tf2_ros.TransformBroadcaster()
+            t = geometry_msgs.msg.TransformStamped()
+            t.header.stamp = rospy.Time.now()
+            t.header.frame_id = "camera_depth_frame2"
+            t.child_frame_id = "obj" + str(i)
+
+            # putting world coordinates coordinates as viewed for sjcam frame
+            # t.transform.translation.x = world_z
+            # t.transform.translation.y = -world_x
+            # t.transform.translation.z = world_y
+
+            t.transform.translation.x = world_x
+            t.transform.translation.y = world_y
+            t.transform.translation.z = world_z
+
+            # not extracting any orientation thus orientation is (0, 0, 0)
+            q = tf_conversions.transformations.quaternion_from_euler(0, 0, 0)
+            t.transform.rotation.x = q[0]
+            t.transform.rotation.y = q[1]
+            t.transform.rotation.z = q[2]
+            t.transform.rotation.w = q[3]
+
+            br.sendTransform(t)
+
+            coordinate.append(world_z)
+            coordinate.append(-world_x)
+            coordinate.append(world_y)
+            coordinates.append(coordinate)
+            coordinate = []
+
+            cv2.putText(rgb_frame, 'obj{}'.format(i),(cX, cY-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.circle(rgb_frame, (cX, cY), 2, (255, 255, 255), -1)
+            cv2.circle(depth_frame, (cX, cY), 2, (0, 255, 0), -1)
+
+            i += 1
+
+        cv2.imshow("frame", rgb_frame)
+        cv2.waitKey(1)
+
+        return coordinates
+
+
+    def image_callback(self, rgb_data, depth_data):
+
         try:
-            frame = self.bridge.imgmsg_to_cv2(rgb_data, "bgr8")
-            depth_frame = self.bridge.imgmsg_to_cv2(depth_data, "32FC1")
-            depth_array = np.array(depth_frame, dtype=np.float32)
-
-            img_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            low_HSV = np.array([0, 53, 137])
-            upper_HSV = np.array([10, 255, 255])
-
-            mask = cv2.inRange(img_HSV, low_HSV, upper_HSV)
-            output = cv2.bitwise_and(frame, frame, mask = mask)
-            output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-
-            ret, threshold = cv2.threshold(output, 25, 255, cv2.THRESH_BINARY)
-
-            contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            # cv2.drawContours(frame, contours, -1, (255, 255, 255), 2)
-
-            i=0
-
-            for contour in contours:
-                C = cv2.moments(contour)
-
-                cX = int(C['m10']/(C['m00']+1e-4))
-                cY = int(C['m01']/(C['m00']+1e-4))
-                depth = depth_frame[cY][cX]
-
-                # transforming pixel coordinates to world coordinates
-                if depth < 1:
-                    world_x = (cX - center_x)/focal_length*depth
-                    world_y = (cY - center_y)/focal_length*depth
-                    world_z = depth
-
-                    # broadcasting TF for each tomato coordinate
-                    br = tf2_ros.TransformBroadcaster()
-                    t = geometry_msgs.msg.TransformStamped()
-                    t.header.stamp = rospy.Time.now()
-                    t.header.frame_id = "camera_link2"
-                    t.child_frame_id = "obj"+str(i)
-
-                    # putting world coordinates coordinates as viewed for sjcam frame
-                    t.transform.translation.x = world_z
-                    t.transform.translation.y = -world_x
-                    t.transform.translation.z = world_y
-
-                    # not extracting any orientation thus orientation is (0, 0, 0)
-                    q = tf_conversions.transformations.quaternion_from_euler(0, 0, 0)
-                    t.transform.rotation.x = q[0]
-                    t.transform.rotation.y = q[1]
-                    t.transform.rotation.z = q[2]
-                    t.transform.rotation.w = q[3]
-
-                    br.sendTransform(t)
-
-                cv2.putText(frame, 'obj{}'.format(i),(cX, cY-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                cv2.circle(frame, (cX, cY), 2, (255, 255, 255), -1)
-                cv2.circle(depth_frame, (cX, cY), 2, (0, 255, 0), -1)
-
-                i+=1
-
-            cv2.imshow("frame_2", frame)
-            # cv2.imshow("depth_frame", depth_frame)
-            cv2.waitKey(1)
+            self.rgb_frame = self.bridge.imgmsg_to_cv2(rgb_data, "bgr8")
+            self.depth_frame = self.bridge.imgmsg_to_cv2(depth_data, "32FC1")
 
         except CvBridgeError as e:
             print(e)
-
-    # Destructor
-    # def __del__(self):
-    #     moveit_commander.roscpp_shutdown()
-    #     rospy.loginfo(
-    #         '\033[94m' + "Object of class Ur5Moveit Deleted." + '\033[0m')
-
-
-# def main():
-#
-#     ur5 = Ur5Moveit()
-
-    # ur5_pose_1 = geometry_msgs.msg.Pose()
-    # ur5_pose_1.position.x = -0.817261772949
-    # ur5_pose_1.position.y = -0.109110076352
-    # ur5_pose_1.position.z = 0.94446979642
-    # ur5_pose_1.orientation.x = -0.999999995957
-    # ur5_pose_1.orientation.y = 4.37354574363e-05
-    # ur5_pose_1.orientation.z = 7.85715579538e-05
-    # ur5_pose_1.orientation.w = 2.12177767514e-09
-    #
-    # ur5_pose_2 = geometry_msgs.msg.Pose()
-    # ur5_pose_2.position.x = -0.414925357653
-    # ur5_pose_2.position.y = 0.284932768677
-    # ur5_pose_2.position.z = 1.78027849967
-    # ur5_pose_2.orientation.x = -0.199396929724
-    # ur5_pose_2.orientation.y = 1.64394297608e-05
-    # ur5_pose_2.orientation.z = 0.979918803013
-    # ur5_pose_2.orientation.w = 6.03911583936e-05
-    #
-    # ur5_pose_3 = geometry_msgs.msg.Pose()
-    # ur5_pose_3.position.x = 0.061218702528
-    # ur5_pose_3.position.y = 0.150917431354
-    # ur5_pose_3.position.z = 1.20083763657
-    # ur5_pose_3.orientation.x = 0.635613875737
-    # ur5_pose_3.orientation.y = 0.77190802743
-    # ur5_pose_3.orientation.z = 0.00233308772292
-    # ur5_pose_3.orientation.w = 0.0121472162087
-
-    # while not rospy.is_shutdown():
-    #     ur5.go_to_pose(ur5_pose_1)
-    #     rospy.sleep(2)
-    #     ur5.go_to_pose(ur5_pose_2)
-    #     rospy.sleep(2)
-    #     ur5.go_to_pose(ur5_pose_3)
-    #     rospy.sleep(2)
-
-    # del ur5
-
 
 if __name__ == '__main__':
     ur5 = Ur5Moveit()
